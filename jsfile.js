f6e76a9ad76687707c603d4deba0263a0895bc2d
Sanchit <sk010us@gmail.com>
2025-04-24 13:15:44 -0700

    added color and reset button

--- jsfile.js
+++ jsfile.js
@@ -2,9 +2,35 @@ const container = document.getElementById('container');
 
 const gridSize = 16;
 const totalSquares = gridSize * gridSize;
+let r = 0;
+let g = 0;
+let b = 0;
+function getRandomNum(min, max) {
+    return Math.random() * (max - min) + min;
+  };  
 
 for (let i = 0; i < totalSquares; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+    
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        let re = getRandomNum(0, 255);
+        let gr = getRandomNum(0, 255);
+        let bl = getRandomNum(0, 255);
+        square.style.backgroundColor = `rgb(${r+re}, ${g+gr}, ${b+bl})`;
+    });
+
+    container.appendChild(square);
+  
 }
+// Add a button to reset the grid
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener('click', () => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.style.backgroundColor = 'white'; // or any default color you want
+  });
+});
+
